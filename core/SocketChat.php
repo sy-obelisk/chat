593e7778f474b3b7e4b5864bdc2908c1de76aac0
Obelisk <352627129@qq.com>
2017-06-27 11:26:04 +0800

    test

--- core/SocketChat.php
+++ core/SocketChat.php
@@ -26,6 +26,7 @@ class SocketChat
     public function startServer()
     {
         $this->master = socket_create_listen( $this->port );
+        var_dump($this->master);die;
         if( !$this->master ) throw new \ErrorException("listen {$this->port} fail !");
         $this->runLog("Server Started : ".date('Y-m-d H:i:s'));
         $this->runLog("Listening on   : 127.0.0.1 port " . $this->port);
@@ -36,7 +37,6 @@ class SocketChat
             //阻塞接收客户端链接
             @socket_select( $readFds, $writeFds, $e = null, $this->timeout );
             foreach( $readFds as $socket ){
-                var_dump(111);die;
                 //当前链接 是主进程
                 if( $this->master == $socket ){
 
