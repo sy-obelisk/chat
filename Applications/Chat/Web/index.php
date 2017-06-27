0b3fc6032ce114205fa1ab2721d0990d84a02025
Obelisk <352627129@qq.com>
2017-06-27 14:15:58 +0800

    test

--- Applications/Chat/Web/index.php
+++ Applications/Chat/Web/index.php
@@ -153,13 +153,6 @@
                &nbsp;&nbsp;&nbsp;&nbsp;<a href="/?room_id=1">房间1</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="/?room_id=2">房间2</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="/?room_id=3">房间3</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="/?room_id=4">房间4</a>
                <br><br>
                </div>
-               <p class="cp">PHP多进程+Websocket(HTML5/Flash)+PHP Socket实时推送技术&nbsp;&nbsp;&nbsp;&nbsp;Powered by <a href="http://www.workerman.net/workerman-chat" target="_blank">workerman-chat</a></p>
-	        </div>
-	        <div class="col-md-3 column">
-	           <div class="thumbnail">
-                   <div class="caption" id="userlist"></div>
-               </div>
-               <a href="http://workerman.net:8383" target="_blank"><img style="width:252px;margin-left:5px;" src="/img/workerman-todpole.png"></a>
 	        </div>
 	    </div>
     </div>
